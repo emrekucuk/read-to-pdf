d4fe7c1f954767561cfe91c033036b031d950ea6
Emre Küçük <emrekucuk74@hotmail.com>
2022-11-02 17:43:48 +0300

    Created Postgresql Connection

---
+++ Databases/DbContext.cs
@@ -0,0 +1,62 @@
+using System.Data.Common;
+using Npgsql;
+
+namespace Databases;
+
+public class DbContext
+{
+    public static string connectionString = "User ID=postgres;Password=password;Server=localhost;Port=5432;Database=currency-exchange;Integrated Security=true;Pooling=true;";
+    public DbConnection Connection()
+    {
+
+        var connection = new NpgsqlConnection(connectionString);
+        return connection;
+    }
+
+    public void InstertData(string filePath, string pageText)
+    {
+        using (var connection = new NpgsqlConnection(connectionString))
+
+        {
+            Console.Out.WriteLine("Opening connection");
+            connection.Open();
+
+            var createCommand = new NpgsqlCommand("INSERT INTO searchablecontents (bookid, content, filename) VALUES (@b, @c,@f) returning id", connection);
+
+            var guid = Guid.NewGuid();
+            createCommand.Parameters.AddWithValue("b", guid);
+            createCommand.Parameters.AddWithValue("c", pageText);
+            createCommand.Parameters.AddWithValue("f", filePath);
+
+            var id = createCommand.ExecuteScalar();
+
+            System.Console.WriteLine($"----------Insert----------");
+
+            var updateCommand = new NpgsqlCommand($"UPDATE searchablecontents s1 SET tokens = to_tsvector('Turkish', s1.content) WHERE id = {id}", connection);
+
+            updateCommand.ExecuteNonQuery();
+            System.Console.WriteLine($"----------Update----------");
+        }
+    }
+
+
+    // public void GetAllCourses()
+    // {
+    //     var connection = Connection();
+    //     connection.Open();
+
+    //     DbCommand command = connection.CreateCommand();
+
+    //     command.CommandText = "select * from \"Courses\"";
+
+
+    //     var reader = command.ExecuteReader();
+    //     while (reader.Read())
+    //     {
+    //         Console.WriteLine(
+    //             string.Format($"Reading from table={reader.GetString(1)}"));
+    //     }
+    //     reader.Close();
+    // }
+
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,26 @@
-﻿var filePaths = FileUtilites.FileUtilities.GetAllFileFromDirectory("pdfs");
+﻿
+using System.Data.Common;
+
+// DbConnection connection = Databases.DbContext.Connection();
+
+// System.Console.WriteLine(connection.State);
+// connection.Open();
+// System.Console.WriteLine(connection.State);
+
+var dbContext = new Databases.DbContext();
+
+var filePaths = FileUtilites.FileUtilities.GetAllFileFromDirectory("pdfs");
 
 filePaths.ForEach((filePath) =>
 {
     System.Console.WriteLine(filePath);
     var pageText = PdfUtilities.PdfUtilities.ReadFile(filePath);
 
-    System.Console.WriteLine(pageText);
+    dbContext.InstertData(filePath, pageText);
+
+    // dbContext.GetAllCourses();
+    // System.Console.WriteLine(pageText);
     System.Console.WriteLine("------------------------------------------------------------------------------------------------------------------------------------------");
 });
+
+
